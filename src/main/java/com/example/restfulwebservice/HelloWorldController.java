bb6655faf5b25a5eea8fca01f278ad5585271b17
Gichul Roh <gichulroh@gmail.com>
2020-08-05 23:35:01 +0900

    first commit

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (endpoint)
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    // @GetMapping은 스프링 4.0 이상 지원, 기존에는 @RequestMapping 사용
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    // alt + enter
+    // 자바 빈으로 리턴값을 돌리면 @RestController는 JSON형태로 변환하여 반환함
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+}
